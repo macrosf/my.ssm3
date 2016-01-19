f968e3ca083c0fb8675441a10849eb128df1a001
macrosf <macros_f@163.com>
2016-01-19 17:27:25 +0800

    add DAO, service layer

--- my.ssm3/src/main/java/cn/oss/ssm3/spitter/service/SpitterServiceImpl.java
+++ my.ssm3/src/main/java/cn/oss/ssm3/spitter/service/SpitterServiceImpl.java
@@ -3,37 +3,31 @@ package cn.oss.ssm3.spitter.service;
 import static java.lang.Math.*;
 import static java.util.Collections.*;
 
+import java.util.Date;
 import java.util.List;
 
-import org.springframework.transaction.TransactionStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.support.TransactionCallback;
-import org.springframework.transaction.support.TransactionTemplate;
 
 import cn.oss.ssm3.spitter.domain.Spitter;
 import cn.oss.ssm3.spitter.domain.Spittle;
 import cn.oss.ssm3.spitter.persistence.SpitterDao;
 
-
+/**
+ * copy from project [spitter-service-annotation-tx]
+ * @author Administrator
+ *
+ */
+@Service("spitterService")
 @Transactional(propagation=Propagation.REQUIRED)
 public class SpitterServiceImpl implements SpitterService {
 
-  //<start id="java_addSpittle" /> 
-  public void saveSpittle(final Spittle spittle) {
-    txTemplate.execute(new TransactionCallback<Void>() {
-      public Void doInTransaction(TransactionStatus txStatus) {
-        try {
-        spitterDao.saveSpittle(spittle);
-        } catch (RuntimeException e) {
-          txStatus.setRollbackOnly();
-          throw e;
-        }
-        return null;
-      }      
-    });
+  public void saveSpittle(Spittle spittle) {
+    spittle.setWhen(new Date());
+    spitterDao.saveSpittle(spittle);
   }
-  //<end id="java_addSpittle" />
 
   @Transactional(propagation=Propagation.SUPPORTS, readOnly=true)
   public List<Spittle> getRecentSpittles(int count) {
@@ -46,64 +40,48 @@ public class SpitterServiceImpl implements SpitterService {
             min(49, recentSpittles.size()));
   }
   
-  public void saveSpitter(final Spitter spitter) {
-    txTemplate.execute(new TransactionCallback<Void>() {
-      public Void doInTransaction(TransactionStatus txStatus) {
-        if(spitter.getId() == null) {
-          spitterDao.addSpitter(spitter);
-        } else {
-          spitterDao.saveSpitter(spitter);
-        }
-
-        return null;
-      }
-    });
+  public void saveSpitter(Spitter spitter) {
+    if(spitter.getId() == null) {
+      spitterDao.addSpitter(spitter);
+    } else {
+      spitterDao.saveSpitter(spitter);
+    }
   }
   
+  @Transactional(propagation=Propagation.SUPPORTS, readOnly=true)
   public Spitter getSpitter(long id) {
-    // TODO Auto-generated method stub
-    return null;
+    return spitterDao.getSpitterById(id);
+  }
+
+  public void startFollowing(Spitter follower, Spitter followee) {
+    // TODO Auto-generated method stub  
   }
   
+  public List<Spittle> getSpittlesForSpitter(Spitter spitter) {
+    return spitterDao.getSpittlesForSpitter(spitter);
+  }
+
+  public List<Spittle> getSpittlesForSpitter(String username) {
+    Spitter spitter = spitterDao.getSpitterByUsername(username);
+    return getSpittlesForSpitter(spitter);
+  }
+
   public Spitter getSpitter(String username) {
     return spitterDao.getSpitterByUsername(username);
   }
 
-  public void startFollowing(Spitter follower, Spitter followee) {
-    // TODO Auto-generated method stub
-    
-  }
-  
-  public List<Spittle> getSpittlesForSpitter(
-          Spitter spitter) {
-    return spitterDao.getSpittlesForSpitter(spitter);
-  }
-  
   public void deleteSpittle(long id) {
     spitterDao.deleteSpittle(id);
   }
   
-  public Spittle getSpittleById(long id) {
-    return spitterDao.getSpittleById(id);
-  }
-  
-  private SpitterDao spitterDao;
-  public void setSpitterDao(SpitterDao spitterDao) {
-    this.spitterDao = spitterDao;
-  }
-  
-  private TransactionTemplate txTemplate;
-  public void setTransactionTemplate(TransactionTemplate txTemplate) {
-    this.txTemplate = txTemplate;
-  }
-
-  public List<Spittle> getSpittlesForSpitter(
-          String username) {
-    // TODO Auto-generated method stub
-    return null;
-  }
-  
   public List<Spitter> getAllSpitters() {
     return spitterDao.findAllSpitters();
+  }
+
+  @Autowired
+  SpitterDao spitterDao;
+
+  public Spittle getSpittleById(long id) {
+    return spitterDao.getSpittleById(id);
   }
 }
